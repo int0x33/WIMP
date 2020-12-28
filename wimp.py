3c3080d2c4ee5354f01a13c1d85701908c8ef93c
ZER0 <46683415+int0x33@users.noreply.github.com>
2020-12-28 12:51:52 -0800

    Create wimp.py

---
+++ wimp.py
@@ -0,0 +1,20 @@
+from pocket import Pocket, PocketException
+import json
+import os
+
+// Sign up to pocket API then get tokens here: https://reader.fxneumann.de/plugins/oneclickpocket/auth.php
+p = Pocket(
+consumer_key='',
+access_token=''
+)
+
+# Fetch a list of articles
+try:
+    lis = p.retrieve(offset=0, count=1000)
+    for i in lis['list']:
+        if "github.com" in lis['list'][i]['given_url']:
+            print(lis['list'][i]['given_url'])
+            os.system("git clone {}".format(lis['list'][i]['given_url']))
+            p.delete(lis['list'][i]['item_id'])
+except PocketException as e:
+    print(e.message)
