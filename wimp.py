7d57a8e71db712cdf805edaf6008e005629d6970
ZER0 <46683415+int0x33@users.noreply.github.com>
2020-12-28 13:22:40 -0800

    Update wimp.py

--- wimp.py
+++ wimp.py
@@ -10,9 +10,9 @@ access_token=''
 
 # Fetch a list of articles
 try:
-    lis = p.retrieve(offset=0, count=1000)
+    lis = p.retrieve(offset=0, count=10000)
     for i in lis['list']:
-        if "github.com" in lis['list'][i]['given_url']:
+        if "https://github.com" in lis['list'][i]['given_url']:
             print(lis['list'][i]['given_url'])
             os.system("git clone {}".format(lis['list'][i]['given_url']))
             p.delete(i)
